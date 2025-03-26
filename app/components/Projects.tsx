d6aa1112c2ee8a929458596e2e46e68bb8a6c09f
Lafiasabigado <lafiasabigado97@gmail.com>
2025-03-26 12:37:49 +0100

    edit projects section

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,7 +1,6 @@
 "use client";
-
 import * as React from "react";
-import Link from "next/link"
+import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
@@ -12,6 +11,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
 
 export function Projects() {
   const projects = [
@@ -19,95 +19,121 @@ export function Projects() {
       id: 1,
       name: "Dieu Existe Center",
       image: "/projects/Dieuexistecenter.jpg",
-      link:"",
+      link: "",
       title: "Dieu Existe Center",
       description: "Site web de formation en ligne dans la musique.",
+      technologies: ["Php", "Bootstrap"]
     },
     {
       id: 2,
       name: "Tech Hive",
       image: "/projects/techhive.jpg",
-      link:"",
+      link: "",
       title: "Tech Hive",
       description: "Site de vente d'ordinateur en ligne.",
+      technologies: ["Php", "Bootstrap", "Laravel"]
     },
     {
       id: 3,
       name: "Task Manage App",
       image: "/projects/habitude.png",
-      link:"/projects/habitude.mp4",
+      link: "/projects/habitude.mp4",
       title: "Task Manage App",
       description: "Application mobile de Gestion de tâche.",
+      technologies: ["React Native","Firebase"]
     },
-    
     {
       id: 4,
-      name: "Project 5",
+      name: "Food mobile App",
       image: "/projects/project2.jpg",
-      link:"",
+      link: "",
       title: "Food mobile App",
       description: "Application mobile de commande de repas.",
+      technologies: ["Flutter", "Dart", "Firebase"]
     },
   ];
 
-  // État pour suivre l'index de la slide active
-  const [activeIndex, setActiveIndex] = React.useState(0);
-
-  // Défilement automatique toutes les 5 secondes
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      setActiveIndex((prevIndex) => (prevIndex + 1) % projects.length); // Passer à la slide suivante
-    }, 2000);
-
-    // Nettoyer l'intervalle lorsque le composant est démonté
-    return () => clearInterval(interval);
-  }, [projects.length]);
+  const plugin = React.useRef(
+    Autoplay({ delay: 2000, stopOnInteraction: true })
+  );
 
   return (
-    <section id="projects" className="max-w-5xl mx-auto pt-16 px-4 sm:px-6 lg:px-8 overflow-hidden">
+    <section 
+      id="projects" 
+      className="max-w-5xl mx-auto pt-16 px-4 sm:px-6 lg:px-8 overflow-hidden"
+    >
       <div className="flex flex-col text-center mt-2">
         <h2 className="text-xl font-bold uppercase">Mes Projets</h2>
         <p className="text-gray-500 text-sm text-center">
           Découvrez un aperçu de mes projets.
         </p>
       </div>
+      
       <Carousel
+        plugins={[plugin.current]}
         opts={{
           align: "center",
-          startIndex: activeIndex, 
+          loop: true,
         }}
         className="w-full max-w-3xl mx-auto mt-4"
       >
         <CarouselContent>
           {projects.map((project) => (
-            <CarouselItem key={project.id} className="md:basis-1/2 lg:basis-1/3">
-              <div className="p-1">
-                <Card className="border-none bg-gray-700/50 rounded-2xl hover:bg-gray-800/50 h-[370px] w-full">
-                  <CardContent className="flex flex-col aspect-square justify-center p-6">
+            <CarouselItem 
+              key={project.id} 
+              className="md:basis-1/2 lg:basis-1/3 p-1"
+            >
+              <Card className="border-none bg-gray-700/50 rounded-2xl hover:bg-gray-800/50 h-full">
+                <CardContent className="flex flex-col justify-between p-6 h-full">
+                  <div className="relative w-full h-48 mb-4">
                     <Image
                       src={project.image}
                       alt={project.name}
-                      width={500}
-                      height={500}
-                      className="w-full h-36 rounded-2xl border object-cover transition-transform duration-300 group-hover:scale-105"
+                      fill
+                      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                      className="object-cover rounded-xl 
+                        transition-transform 
+                        duration-500 
+                        ease-in-out 
+                        group-hover:scale-110"
+                      priority={false}
                     />
-                    <div className="flex pt-2 flex-col justify-start items-start my-2">
-                      <h3 className="text-lg font-bold my-1">{project.title}</h3>
-                      <p className="text-gray-300 my-1">{project.description}</p>
-                      <div className="pt-2">
-                        <Link href={project.link}>
-                         <Button className="bg-indigo-500 hover:bg-indigo-600 text-white px-8 py-2 rounded-full">
-                          Voir Démo
-                         </Button>
-                        </Link>
-                      </div>
+                  </div>
+                  
+                  <div className="flex flex-col justify-start items-start">
+                    <h3 className="text-lg font-bold my-1">{project.title}</h3>
+                    <p className="text-gray-300 my-1 text-sm">
+                      {project.description}
+                    </p>
+                    
+                    {/* Technologies section */}
+                    <div className="flex flex-wrap gap-2 my-2">
+                      {project.technologies.map((tech) => (
+                        <span 
+                          key={tech} 
+                          className="text-xs bg-gray-800 text-gray-300 px-2 py-1 rounded-full"
+                        >
+                          {tech}
+                        </span>
+                      ))}
                     </div>
-                  </CardContent>
-                </Card>
-              </div>
+                    
+                    <div className="pt-2 w-full">
+                      <Link href={project.link} className="w-full">
+                        <Button 
+                          className="bg-indigo-500 hover:bg-indigo-600 text-white px-8 py-2 rounded-full w-full"
+                        >
+                          Voir Démo
+                        </Button>
+                      </Link>
+                    </div>
+                  </div>
+                </CardContent>
+              </Card>
             </CarouselItem>
           ))}
         </CarouselContent>
+        
         <CarouselPrevious />
         <CarouselNext />
       </Carousel>
--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -4,7 +4,6 @@ import { FaFlutter } from 'react-icons/fa6'
 import { FaHtml5 } from 'react-icons/fa6'
 import { FaCss3Alt } from 'react-icons/fa6'
 import { FaJs } from 'react-icons/fa6'
-import { FaPython } from 'react-icons/fa6'
 import { FaLaravel } from 'react-icons/fa6'
 import { FaReact } from 'react-icons/fa6'
 import { FaBootstrap } from 'react-icons/fa6'
@@ -15,7 +14,6 @@ import { FaJava } from 'react-icons/fa6'
 import { SiDart } from 'react-icons/si'
 
 const Skills = () => {
-
   const skillsweb = [
     {id:1 ,name: "Php", icon: <FaPhp className="text-blue-500" />},
     { id: 2, name: "HTML", icon: <FaHtml5 className="text-orange-500" /> },
@@ -36,10 +34,13 @@ const Skills = () => {
   ]
   return (
     <section id='skills' className='max-w-3xl mx-auto pt-16 rounded-lg'>
-      <h2 className='text-xl font-bold mb-3 text-center uppercase'>Mes Compétences</h2>
+      <div className='flex flex-col text-center mt-2'>
+        <h2 className='text-xl font-bold uppercase'>Mes Compétences</h2>
+        <p className='text-gray-500 text-sm text-center'>Découvrez mes compétences.</p>
+      </div>
       <div className='grid grid-cols-1 md:grid-cols-2 gap-4 pt-6'>
-        <div className='flex flex-col gap-2 h-full w-full '>
-          <div className='h-[350px] w-full bg-gray-800/50 hover:bg-gray-700/30 rounded-2xl p-6 mx-2 flex flex-col justify-between'>
+        <div className='flex justify-center'>
+          <div className='h-full w-[350px] bg-gray-800/50 hover:bg-gray-700/30 rounded-2xl p-6 flex flex-col justify-between'>
             <p className='text-gray-300 font-mono text-xl'>
                Développement web
             </p>
@@ -51,29 +52,29 @@ const Skills = () => {
               <div key={skill.id} className='flex hover:bg-gray-600/50 transition-colors duration-200 items-center justify-around gap-2 border border-gray-600 rounded-full p-2'>
                   {skill.icon}
                 <span className='text-gray-300'>{skill.name}</span>
-              </div>
+                </div>
               ))}
             </div>
           </div>
         </div>
-        <div className='flex flex-col gap-2 h-full w-full'>
-          <div className='h-[350px] w-full md:bg-gray-800/50 md:hover:bg-gray-700/30 bg-gray-600/50 hover:bg-gray-700/30 rounded-2xl p-6 mx-2 flex flex-col justify-between'>
+        <div className='flex justify-center'>
+          <div className='h-full w-[350px] bg-gray-800/50 hover:bg-gray-700/30 rounded-2xl p-6 flex flex-col justify-between'>
             <p className='text-gray-300 font-mono text-xl'>
                Développement mobile
             </p>
-              <p className='text-gray-300 py-2'>
-                Mon expérience avec React Native et Flutter m'a permis de développer des applications mobiles multiplateformes fonctionnant parfaitement sur iOS et Android.            
-              </p>
-              <div className='flex flex-wrap gap-2 py-6 rounded-2xl'>
-                {skillsmobile.map((skill) => (
-                <div key={skill.id} className='flex hover:bg-gray-600/50 transition-colors duration-200 items-center justify-around gap-2 border border-gray-600 rounded-full p-2'>
-                  {skill.icon}
+            <p className='text-gray-300 py-2'>
+              Mon expérience avec React Native et Flutter m'a permis de développer des applications mobiles multiplateformes fonctionnant parfaitement sur iOS et Android.            
+            </p>
+            <div className='flex flex-wrap gap-2 py-6 rounded-2xl'>
+              {skillsmobile.map((skill) => (
+              <div key={skill.id} className='flex hover:bg-gray-600/50 transition-colors duration-200 items-center justify-around gap-2 border border-gray-600 rounded-full p-2'>
+                {skill.icon}
                 <span className='text-gray-300'>{skill.name}</span>
                 </div>
-                ))}
-              </div>
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
       </div>
     </section>
   )
